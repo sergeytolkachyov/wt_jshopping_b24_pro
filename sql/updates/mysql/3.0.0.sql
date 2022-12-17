b5417b1537574be114ec03ad5792bb2b7149ef99
Sergey Tolkachyov <musstudent@mail.ru>
2022-12-17 09:49:10 +0400

    скобка на новой строке вызывала предупреждение в админке джумлы при проверке базы данных о том, что "таблица не найдена"

--- sql/updates/mysql/3.0.0.sql
+++ sql/updates/mysql/3.0.0.sql
@@ -1,5 +1,4 @@
-CREATE TABLE IF NOT EXISTS `#__wt_jshopping_bitrix24_pro_products_relationship`
-(
+CREATE TABLE IF NOT EXISTS `#__wt_jshopping_bitrix24_pro_products_relationship` (
     `jshopping_product_id` int(11) UNIQUE,
     `bitrix24_product_id`  int(11)
 ) DEFAULT CHARSET = utf8;
